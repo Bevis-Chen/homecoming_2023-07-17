39b03cf4b30e686b3df71c2c426017a9776aac0d
Austin <Austin@gmail.com>
2023-07-18 00:17:25 +0800

    進度落後@@

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
+from crawler.pm25 import get_pm25
 
 app= Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h2>Hello Flask!</h2>"
 
+    return render_template("index.html")
+
+@app.route("/pm25")
+def pm25values():
+    # 檢查是否有勾選
+    sort= request.args.get("sort")
+    print(sort)
+    columns, values= get_pm25(sort)
+    return render_template("pm25.html", sort= sort, columns= columns, values= values)
 
 @app.route("/nowtime")
 def get_nowtime():
